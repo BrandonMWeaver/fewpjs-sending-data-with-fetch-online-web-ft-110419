15493b704d62366d188376d22e285079eaf8fd1b
Brandon Weaver <a7xncob@gmail.com>
2020-02-23 20:42:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ function submitData(name, email)
       "Accept": "application/json"
     },
     body: JSON.stringify({
-      dogName: "Byron",
-      dogBreed: "Poodle"
+      name: name,
+      email: email
     })
   }).then(function(response) {
     return response.json();
