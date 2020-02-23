c6a6a32350726e5d6e70b42e4f26746a3d2b87ed
Brandon Weaver <a7xncob@gmail.com>
2020-02-23 20:41:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,22 +1,20 @@
-fetch("http://localhost:3000/dogs", {
-  method: "POST",
-  headers: {
-    "Content-Type": "application/json",
-    "Accept": "application/json"
-  },
-  body: JSON.stringify({
-    dogName: "Byron",
-    dogBreed: "Poodle"
-  })
-}).then(function(response) {
-  return response.json();
-}).then(function(object) {
-  console.log(object);
-}).catch(function(error) {
-  console.log(error.message);
-});
-
 function submitData(name, email)
 {
-  
+  return fetch("http://localhost:3000/dogs", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify({
+      dogName: "Byron",
+      dogBreed: "Poodle"
+    })
+  }).then(function(response) {
+    return response.json();
+  }).then(function(object) {
+    console.log(object);
+  }).catch(function(error) {
+    console.log(error.message);
+  });
 }
