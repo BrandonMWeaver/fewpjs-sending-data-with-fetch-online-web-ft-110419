7098989362a2f4dd1fd0f920a108e0025cd866c0
Brandon Weaver <a7xncob@gmail.com>
2020-02-23 20:45:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,6 +15,6 @@ function submitData(name, email)
   }).then(function(object) {
     console.log(object);
   }).catch(function(error) {
-    console.log(error.message);
+    document.body.innerHTML = error.message;
   });
 }
