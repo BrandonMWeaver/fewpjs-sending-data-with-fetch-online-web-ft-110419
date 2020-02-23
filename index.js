232e0fcee1dcec9fea815fcb8c833c58d4d4c0d7
Brandon Weaver <a7xncob@gmail.com>
2020-02-23 20:34:07 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,11 @@
+fetch("http://localhost:3000/dogs", {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+    "Accept": "application/json"
+  },
+  body: JSON.stringify({
+    dogName: "Byron",
+    dogBreed: "Poodle"
+  })
+});
